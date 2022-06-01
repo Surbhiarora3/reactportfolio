612465d31f91d2638b78cbc8106f1da5afd4c864
Surbhi Arora <arora.surbhi4@gmail.com>
2022-05-31 19:59:05 -0700

    Updates

---
+++ precache-manifest.cf0ccaec81882476f8534f12efc3841e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ec4f200398899a5ee7c",
+    "url": "/reactportfolio/static/css/main.ce9170e1.chunk.css"
+  },
+  {
+    "revision": "4ec4f200398899a5ee7c",
+    "url": "/reactportfolio/static/js/main.4ec4f200.chunk.js"
+  },
+  {
+    "revision": "aa08bf25ad038a4f2164",
+    "url": "/reactportfolio/static/js/1.aa08bf25.chunk.js"
+  },
+  {
+    "revision": "f3444e3a511a2aa26237",
+    "url": "/reactportfolio/static/js/runtime~main.f3444e3a.js"
+  },
+  {
+    "revision": "72ba2721ef692248f9083f6eb11fd730",
+    "url": "/reactportfolio/index.html"
+  }
+];
